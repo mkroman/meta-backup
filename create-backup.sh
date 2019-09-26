d7479fdc66cf198c96d2e08b84db0042201f49b8
Mikkel Kroman <mk@uplink.io>
2019-09-26 20:52:02 +0200

    Fix docker login error

--- create-backup.sh
+++ create-backup.sh
@@ -4,4 +4,4 @@ BACKUP_SOURCE=${BACKUP_SOURCE:-/data}
 
 echo "Creating backup"
 
-# restic backup --tag automatic --host meta "${BACKUP_SOURCE}"
+restic backup --tag automatic --host meta "${BACKUP_SOURCE}"
